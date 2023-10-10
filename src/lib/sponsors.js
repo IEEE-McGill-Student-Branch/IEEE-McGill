436848ef0d8a229a36579f7a38be7a5e027ca338
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-10-10 17:12:02 -0400

    Added Stripo sponsor logo

--- src/lib/sponsors.js
+++ src/lib/sponsors.js
@@ -1,9 +1,15 @@
 import IEEE_Montreal from "$images/sponsors/ieee-montreal.png";
+import StripoLogo from "$images/sponsors/stripo.png";
 
 export const sponsors = [  
   {
     name: "IEEE Montréal",
     logo: IEEE_Montreal,
     link: "https://montreal.ieee.ca",
+  },
+  {
+    name: "Stripo",
+    logo: StripoLogo,
+    link: "https://stripo.email",
   },
 ];
