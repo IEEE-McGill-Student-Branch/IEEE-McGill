fedf56db772992e5edffc79f99018650cfd4100b
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-08-31 13:38:10 -0400

    Add pnpm version to package.json + cleanup main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,85 +2,49 @@
   import Logo from "$images/small_logo.png";
   import Section from "$components/Section.svelte";
   import "../app.css";
-  // import IEEE from "$images/sponsors/IEEE-blue.png";
 </script>
 
 <title>IEEE McGill</title>
 
-<div id="content">
-  <div id="introduction">
-    <h1>
-      <img src={Logo} id="logo" alt="IEEE McGill Emblem" class="inline size-16 mx-3"/>
+<div class="space-y-6">
+  <!-- Introduction -->
+  <div class="text-center md:grid md:grid-cols-2 md:items-center">
+    <h1
+      class="text-3xl font-bold p-3 my-2 flex items-center justify-center md:justify-start"
+    >
+      <img src={Logo} alt="IEEE McGill Emblem" class="inline size-16 mx-3" />
       IEEE McGill Student Branch
     </h1>
   </div>
-  <div id="paragraph">
-    <Section>
+
+  <!-- Paragraph Section -->
+  <Section>
+    <p class="text-left leading-relaxed">
       The McGill University IEEE Student Branch was established in April 1968
       and is one of the largest IEEE student branches in Eastern Canada. The
       IEEE student branch at McGill is dedicated to the development of
       professional skills of electrical & computer engineering students and the
       larger academic community.
+    </p>
 
-      <br />
-      <br />
-
+    <p class="text-left leading-relaxed mt-4">
       We are mainly a group of undergraduate and graduate ECE & CS students who
       want to have a great time, create a friendly community, and most
       importantly learn about research and new innovations. We offer a welcoming
       club for networking, volunteerism, teamwork, soft skills, leadership, and
       recognition. We are under the tutelage of the larger IEEE organization and
       organize events and activities such as talks, workshops, competitions,
-      company tours etc. for the benefit of all ECE/CS students.
-      <br />
-      <br />
-      The IEEE organization (Institute of Electrical and Electronics Engineers) and
-      its members inspire a global community to innovate for a better tomorrow through
-      its more than 400,000 members in more than 160 countries, and its highly cited
-      publications, conferences, technology standards, and professional and educational
-      activities. IEEE is considered the trusted “voice” for engineering, computing,
-      and technology information around the globe.
-    </Section>
-  </div>
-</div>
+      company tours, etc. for the benefit of all ECE/CS students.
+    </p>
 
-<!-- <h1>Affiliations</h1>
-<div id="affiliation">
-  <Section>
-    <div class="grid grid-cols-3 items-center">
-      <img src={IEEE} alt="IEEE Blue Logo" id="IEEE" />
-      <span class="col-span-2">
-        IEEE (the Institute of Electrical and Electronics Engineers) is the
-        world's leading professional not-for-profit association for the
-        advancement of technology.
-      </span>
-    </div>
+    <p class="text-left leading-relaxed mt-4">
+      The IEEE organization (Institute of Electrical and Electronics Engineers)
+      and its members inspire a global community to innovate for a better
+      tomorrow through its more than 400,000 members in more than 160 countries,
+      and its highly cited publications, conferences, technology standards, and
+      professional and educational activities. IEEE is considered the trusted
+      “voice” for engineering, computing, and technology information around the
+      globe.
+    </p>
   </Section>
-</div> -->
-
-<style lang="postcss">
-  @reference '../app.css'
-
-  #logo {
-    @apply ;
-  }
-
-  h1 {
-    @apply border-0 p-3 my-2 text-center text-3xl;
-  }
-
-  #content {
-    #introduction {
-      @media screen md {
-        @apply grid grid-cols-2 items-center;
-      }
-    }
-
-    #paragraph {
-      @apply text-left;
-    }
-  }
-  /* #IEEE {
-    @apply w-48;
-  } */
-</style>
+</div>
